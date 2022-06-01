2aa81029c762dd5970e12585daa01b7f08767587
Saumya <saumya071294@gmail.com>
2022-05-31 19:17:25 -0700

    Create find-the-town-judge.py

---
+++ find-the-town-judge.py
@@ -0,0 +1,15 @@
+# T.C. O(n) - number of elements in trust
+# S.C. O(p) - number of person
+class Solution:
+    
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        indeg=[0 for _ in range(n)]
+
+        for a,b in trust:
+            indeg[a-1]-=1
+            indeg[b-1]+=1
+        # print(indeg)
+        for i in range(len(indeg)):
+            if indeg[i]==n-1:
+                return i+1
+        return -1
